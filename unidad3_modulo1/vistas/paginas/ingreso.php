0829dc1d8d50194d5f298a126713a88df5c3d2cc
Maria Paula Vera <mariapaulavera@hotmail.com>
2023-07-14 07:53:34 -0300

    ok

---
+++ unidad3_modulo1/vistas/paginas/ingreso.php
@@ -0,0 +1,23 @@
+<h1 class="tituloPagina">Ingreso</h1>
+<div class="contacto">
+                
+                    <form action="" method="post" class="formulario">
+                       
+                        <p>
+                        <label for="email">Email:</label>
+                        <input type="text" name="ingresoEmail" id="email">
+                        </p>
+                        <p>
+                        <label for="pwd">Contraseña</label>  
+                        <input type="password" name="ingresoPassword" id="pwd">
+                        </p>
+                    
+                        <p class="boton">
+                        <button type="submit">Ingresar</button>
+                        </p>
+                    </form>
+ </div> 
+
+
+</body>
+</html>
---
+++ unidad3_modulo1/vistas/paginas/registro.php
@@ -0,0 +1,27 @@
+<h1 class="tituloPagina">Registro</h1>
+
+<div class="contacto">
+                
+                    <form action="" method="post" class="formulario">
+                        <p>
+                        <label for="nombre">Nombre:</label>
+                        <input type="text" name="registroNombre" id="nombre">
+                        </p>
+                        <p>
+                        <label for="email">Email:</label>
+                        <input type="text" name="registroEmail" id="email">
+                        </p>
+                        <p>
+                        <label for="pwd">Contraseña</label>  
+                        <input type="password" name="registroPassword" id="pwd">
+                        </p>
+                    
+                        <p class="boton">
+                        <input type="submit" name="" id="enviar" value="enviar">
+                        </p>
+                    </form>
+ </div> 
+
+
+</body>
+</html>
