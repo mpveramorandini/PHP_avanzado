ad1546839e034cda6d3a251887233b57132cb20f
Maria Paula Vera <mariapaulavera@hotmail.com>
2023-08-15 15:29:34 -0300

    ok

---
+++ MODULO2/unidad3_M2(clase)ConMiCSS/vistas/paginas/ingreso.php
@@ -0,0 +1,29 @@
+<h1 class="tituloPagina">Ingreso</h1>
+<div class="contacto">
+
+    <form action="" method="post" class="formulario">
+
+        <p>
+            <label for="email">Email:</label>
+            <input type="text" name="ingresoEmail" id="email">
+        </p>
+        <p>
+            <label for="pwd">Contraseña</label>
+            <input type="password" name="ingresoPassword" id="pwd">
+        </p>
+
+	
+
+		<?php 
+
+		$ingreso = new ControladorFormularios();
+		$ingreso -> ctrIngreso();
+
+		?>
+		
+		<p class="boton">
+            <button type="submit">Ingresar</button>
+        </p>
+	</form>
+
+</div>
