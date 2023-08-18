407767693b011f1f07042b388c7d976e5aaae887
Maria Paula Vera <mariapaulavera@hotmail.com>
2023-08-18 17:14:01 -0300

    ok

--- MODULO2/entregaFinal/modelos/conexion.php
+++ MODULO2/entregaFinal/modelos/conexion.php
@@ -2,9 +2,9 @@
 <?php
 class Conexion{
     static public function conectar(){
-        $link = new PDO("mysql:host=localhost;port=3306;dbname=php_avanzado_mpv", 
-                            "root",
-                            ""); 
+        $link = new PDO("mysql:host=localhost;port=3306;dbname=id20761485_php_avanzado_mpv", 
+                            "id20761485_mariapaulavera",
+                            "Torcuato-2010"); 
         $link->exec("set names utf8");
 
         return $link;
